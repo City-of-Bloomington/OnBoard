993e6687579ab0e24c764dda6d7fb198a4fede67
sibow <sibow@9afe1303-3308-a59e-47b3-9f8ac9fa10f0>
2008-09-16 20:39:08 +0000

    git-svn-id: https://civic-legislation.googlecode.com/svn/branches/update@50 9afe1303-3308-a59e-47b3-9f8ac9fa10f0

---
+++ html/topics/viewTopic.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright Copyright (C) 2008 City of Bloomington, Indiana. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @param GET committee_id
+ */
+$topic = new Topic($_GET['topic_id']);
+
+$template = new Template();
+$template->blocks[] = new Block('topics/topicInfo.inc',array('topic'=>$topic));
+/**
+// we will add votes here
+$voteList = new Block('votes/voteList.inc');
+$voteList->voteList = $topic->getVotes();
+$voteList->topic = $topic;
+$template->blocks[] = $voteList;
+*/
+echo $template->render();
