01e3343ebc314e78d55149fa53a8b0ae7ba90ad0
inghamn <inghamn@9afe1303-3308-a59e-47b3-9f8ac9fa10f0>
2009-02-20 20:59:08 +0000

    Login is now in its own directory and linked to from the menubar
    git-svn-id: https://civic-legislation.googlecode.com/svn/trunk@171 9afe1303-3308-a59e-47b3-9f8ac9fa10f0

---
+++ html/login/home.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @copyright 2009 City of Bloomington, Indiana
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ */
+$template = new Template();
+$template->title = 'Login';
+$template->blocks[] = new Block('loginForm.inc');
+echo $template->render();
