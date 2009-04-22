6413943951b9c3030c28664f1687b2c9305fa5f1
inghamn <inghamn@9afe1303-3308-a59e-47b3-9f8ac9fa10f0>
2009-04-22 15:39:52 +0000

    You can now edit past terms for a person by going to their ViewPerson screen.  You cannot edit the past terms from the ViewCommittee->pastMembers
    git-svn-id: https://civic-legislation.googlecode.com/svn/trunk@223 9afe1303-3308-a59e-47b3-9f8ac9fa10f0

--- html/terms/updateTerm.php
+++ html/terms/updateTerm.php
@@ -4,11 +4,17 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  * @param GET term_id
+ * @param REQUEST return_url
  */
-
 verifyUser(array('Administrator','Clerk'));
 
 $term = new Term($_REQUEST['term_id']);
+
+$return_url = isset($_REQUEST['return_url'])
+			? $_REQUEST['return_url']
+			: $term->getSeat()->getCommittee()->getURL();
+
+
 if (isset($_POST['term'])) {
 	foreach ($_POST['term'] as $field=>$value) {
 		$set = 'set'.ucfirst($field);
@@ -17,7 +23,7 @@ if (isset($_POST['term'])) {
 
 	try {
 		$term->save();
-		header('Location: '.$term->getSeat()->getCommittee()->getURL());
+		header('Location: '.$return_url);
 		exit();
 	}
 	catch (Exception $e) {
@@ -30,5 +36,6 @@ $template->title = 'Edit Term';
 $template->blocks[] = new Block('committees/committeeInfo.inc',
 								array('committee'=>$term->getSeat()->getCommittee()));
 $template->blocks[] = new BlocK('seats/seatInfo.inc',array('seat'=>$term->getSeat()));
-$template->blocks[] = new Block('terms/updateTermForm.inc',array('term'=>$term));
+$template->blocks[] = new Block('terms/updateTermForm.inc',
+								array('term'=>$term,'return_url'=>$return_url));
 echo $template->render();
