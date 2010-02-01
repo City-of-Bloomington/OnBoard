aed582c7650e3344ca1c44254761831f0466be36
inghamn <inghamn@9afe1303-3308-a59e-47b3-9f8ac9fa10f0>
2010-02-01 14:10:00 +0000

    Added CAS login support
    git-svn-id: https://civic-legislation.googlecode.com/svn/trunk@277 9afe1303-3308-a59e-47b3-9f8ac9fa10f0

--- html/login/home.php
+++ html/login/home.php
@@ -1,10 +1,38 @@
 <?php
 /**
- * @copyright 2009 City of Bloomington, Indiana
+ *	Logs a user into the system using CAS
+ *
+ * @copyright 2009-2010 City of Bloomington, Indiana
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
-$template = new Template();
-$template->title = 'Login';
-$template->blocks[] = new Block('loginForm.inc');
-echo $template->render();
+if (isset($_REQUEST['return_url'])) {
+	$_SESSION['return_url'] = $_REQUEST['return_url'];
+}
+require_once CAS.'/SimpleCAS/Autoload.php';
+
+$options = array('hostname'=>CAS_SERVER,'uri'=>CAS_URI);
+$protocol = new SimpleCAS_Protocol_Version2($options);
+$client = SimpleCAS::client($protocol);
+$client->forceAuthentication();
+
+if ($client->isAuthenticated()) {
+	try {
+		$user = new User($client->getUsername());
+		$user->startNewSession();
+		setcookie(CAS_COOKIE,'true',0,'/',CAS_DOMAIN);
+
+		if (isset($_SESSION['return_url'])) {
+			header('Location: '.$_SESSION['return_url']);
+		}
+		else {
+			header('Location: '.BASE_URL);
+		}
+	}
+	catch (Exception $e) {
+		$_SESSION['errorMessages'][] = $e;
+	}
+}
+else {
+	header('Location: '.BASE_URL);
+}
