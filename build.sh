0084ff7fa5e012aef6649e2ab144159852248dea
inghamn <inghamn@bloomington.in.gov>
2014-01-28 10:01:42 -0500

    Updated the build script to clean out extraneous files from FontAwesome and YUI

--- build.sh
+++ build.sh
@@ -4,12 +4,14 @@ DIST=./dist
 
 if [ ! -d $BUILD ]
 	then mkdir $BUILD
+	else rm -Rf $BUILD/*
 fi
 
 if [ ! -d $DIST ]
 	then mkdir $DIST
+	else rm -Rf $DIST/*
 fi
 
 rsync -rlv --exclude-from=./buildignore --delete ./ ./build/
 
-tar czvf $DIST/civi-legislation.tar.gz --transform=s/build/civic-legislation/ $BUILD
+tar czvf $DIST/civic-legislation.tar.gz --transform=s/build/civic-legislation/ $BUILD
