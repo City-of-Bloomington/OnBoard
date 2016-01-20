e4a5029a7b7ed0ae0cf4fc7cd99196a4779c0388
inghamn <inghamn@bloomington.in.gov>
2016-01-20 14:22:00 -0500

    Switched date inputs to type="date"
    Updates #42

---
+++ templates/html/helpers/DatePicker.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright 2014-2015 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ */
+namespace Application\Templates\Helpers;
+
+use Blossom\Classes\Helper;
+use Blossom\Classes\View;
+
+class DatePicker extends Helper
+{
+    public function datePicker($fieldname, $id, $timestamp=null)
+    {
+        $date = '';
+        if ($timestamp) {
+            $date = date(DATE_FORMAT, $timestamp);
+        }
+
+        $help = View::translateDateString(DATE_FORMAT);
+        $size = strlen($help);
+
+        return "
+        <input name=\"$fieldname\" id=\"$id\" value=\"$date\"
+            type=\"date\" placeholder=\"$help\" />
+        ";
+    }
+}
