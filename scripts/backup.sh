64dc6a71dd0551a5d3b1649e6e1e83b5fffd3966
inghamn <inghamn@bloomington.in.gov>
2017-09-11 10:32:32 -0400

    Backup now only backs up the database
    The file storage has grown so large that we do not want to
    make nightly tarballs of all the files.  We will rely on
    tape for any restoration of these files.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -20,10 +20,15 @@ num_days_to_keep=5
 now=`date +%s`
 today=`date +%F`
 
+# Only do a nightly snapshot of the database.  Everything is backed up to
+# tape, nightly.  The file storage for OnBoard is so large we do not want
+# to multiply it doing nightly tarballs.  We will rely on our tape storage
+# for any restorations of uploaded files.
+
 # Dump the database
 $MYSQLDUMP --defaults-extra-file=$MYSQL_CREDENTIALS $MYSQL_DBNAME > $SITE_HOME/$MYSQL_DBNAME.sql
 cd $SITE_HOME
-tar czf $today.tar.gz applicantFiles meetingFiles $MYSQL_DBNAME.sql
+tar czf $today.tar.gz $MYSQL_DBNAME.sql
 mv $today.tar.gz $BACKUP_DIR
 
 # Purge any backup tarballs that are too old
