f154e01e0855bf13f5168308c645d3e9615dafed
Cliff Ingham <inghamn@bloomington.in.gov>
2019-08-16 13:17:30 -0400

    Removed CKAN upload ansible deployment

--- ansible/templates/cron.sh
+++ ansible/templates/cron.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
-# @copyright Copyright 2011-2018 City of Bloomington, Indiana
-# @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
+# @copyright Copyright 2011-2019 City of Bloomington, Indiana
+# @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE
 APPLICATION_NAME="onboard"
-PHP="/usr/bin/php"
 MYSQLDUMP="/usr/bin/mysqldump"
 MYSQL_DBNAME="{{ onboard_db.name }}"
 MYSQL_CREDENTIALS="/etc/cron.daily/backup.d/${APPLICATION_NAME}.cnf"
 BACKUP_DIR="{{ onboard_backup_path }}"
 APPLICATION_HOME="{{ onboard_install_path }}"
 SITE_HOME="{{ onboard_site_home }}"
-
-#----------------------------------------------------------
-# Data Warehouse export
-#----------------------------------------------------------
-export SITE_HOME=$SITE_HOME
-{% if onboard_proxy %}
-# Set this if your OnBoard install lives behind a reverse proxy
-export HTTP_X_FORWARDED_HOST={{ onboard_proxy }}
-{% endif %}
-$PHP $APPLICATION_HOME/scripts/Ckan/updateCkan.php
 
 #----------------------------------------------------------
 # Backup
