da2a5f1a27c5204d1aa9522f019fdb96e95829c3
Cliff Ingham <inghamn@bloomington.in.gov>
2023-06-27 13:59:20 -0400

    Fixed full solr indexing script
    The new Solr update required setting a timeout on the Solr object.  Also, if we set REQUEST_URI, then Graylog will tell us what script had an error (when errors occur).

--- scripts/solr/indexSolr.php
+++ scripts/solr/indexSolr.php
@@ -2,17 +2,20 @@
 /**
  * Clear and reindex onboard data in a Solr core
  *
- * @copyright 2021 City of Bloomington, Indiana
+ * @copyright 2021-2023 City of Bloomington, Indiana
  * @license https://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
  */
 declare (strict_types=1);
 
 use Application\Models\MeetingFilesTable;
+use Web\Database;
 use Web\Search\Solr;
 
+$_SERVER['REQUEST_URI'] = __FILE__;
 include '../../bootstrap.php';
 
 $solr   = new Solr($SOLR['onboard']);
+$solr->setTimeout(20);
 $solr->purge();
 $client = $solr->getClient();
 $client->getAdapter()->setTimeout(20);
@@ -21,6 +24,10 @@ $buffer->setBufferSize(100);
 
 $table = new MeetingFilesTable();
 $files = $table->find();
+
+$db    = Database::getConnection();
+$sql   = $db->createStatement('update meetingFiles set indexed=CURRENT_TIMESTAMP where id=?');
+
 $total = count($files);
 $c     = 0;
 foreach ($files as $f) {
@@ -28,6 +35,8 @@ foreach ($files as $f) {
     echo "{$f->getFullPath()} $c/$total\n";
     $data = $solr->prepareIndexFields($f);
     $buffer->createDocument($data);
+
+    $sql->execute([$f->getId()]);
 }
 $buffer->commit();
 
