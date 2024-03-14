3d35ab9f3b43345001f31f6f37d1b0d8d0ef72c3
Cliff Ingham <inghamn@bloomington.in.gov>
2024-03-14 10:27:29 -0400

    Committee::members added actionLinks for each member row

--- src/Web/Committees/Members/SeatedView.php
+++ src/Web/Committees/Members/SeatedView.php
@@ -26,6 +26,10 @@ class SeatedView extends View
             ]
         ];
 
+        if (isset($_SESSION['USER'])) {
+            $this->createActionLinks($committee, $seat_data);
+        }
+
         $this->vars = [
             'committee'   => $committee,
             'seat_data'   => $seat_data,
@@ -36,5 +40,80 @@ class SeatedView extends View
     public function render(): string
     {
         return $this->twig->render("{$this->outputFormat}/committees/seated_members.twig", $this->vars);
+    }
+
+    /**
+     * Adds an actionLinks array to each row of seat_data
+     *
+     * Does permission checks for all possible actions for each row of seat_data
+     */
+    private function createActionLinks(Committee $committee, array &$seat_data)
+    {
+        $actions = [];
+
+        $userCanEditSeats      = parent::isAllowed('seats',      'update');
+        $userCanEditAlternates = parent::isAllowed('alternates', 'update');
+        $userCanAppointMembers = parent::isAllowed('members',    'appoint');
+        $userCanEditOffices    = parent::isAllowed('offices',    'update');
+        $alternates            = $committee->allowsAlternates();
+
+        foreach ($seat_data as $i=>$row) {
+            if ($userCanEditSeats) {
+                $actions[] = [
+                    'url'   => parent::generateUri('seats.update')."?seat_id=$row[seat_id]",
+                    'label' => $this->_('seat_edit')
+                ];
+            }
+            if ($alternates && $userCanEditAlternates) {
+                if ($row['alternate_person_id']) {
+                    $actions[] = [
+                        'url'   => parent::generateUri('alternates.update')."?alternate_id=$row[alternate_id]",
+                        'label' => $this->_('alternate_edit')
+                    ];
+                }
+                else {
+                    $actions[] = [
+                        'url'   => parent::generateUri('alternates.update'),
+                        'label' => $this->_('alternate_add')
+                    ];
+                }
+            }
+            if ($userCanAppointMembers) {
+                if ($row['member_person_id']) {
+                    $uri = $row['seat_type'] === 'termed'
+                            ? parent::generateUri('members.appoint')."?term_id=$row[term_id]"
+                            : parent::generateUri('members.appoint')."?seat_id=$row[seat_id]";
+                    $actions[] = ['url'=>$uri, 'label'=>$this->_('member_add')];
+
+                    if ($row['seat_type'] == 'termed') {
+                        $t = new Term($row['term_id']);
+                        $n = $t->getNextTerm();
+
+                        if ($n->isVacant()) {
+                            $uri = parent::generateUri('members.reappoint')."?member_id=$row[member_id]";
+                            $actions[] = ['url'=>$uri, 'label'=>$this->_('member_continue')];
+                        }
+                    }
+                    if (!$row['member_endDate'] || strtotime($row['member_endDate']) > time()) {
+                        $uri = parent::generateUri('members.resign')."?member_id=$row[member_id]";
+                        $actions[] = ['url'=>$uri, 'label'=>$this->_('member_end')];
+                    }
+                }
+                else {
+                    $uri = $row['seat_type'] === 'termed'
+                            ? parent::generateUri('members.appoint')."?term_id=$row[term_id]"
+                            : parent::generateUri('members.appoint')."?seat_id=$row[seat_id]";
+                    $actions[] = ['url'=>$uri, 'label'=>$this->_('member_add')];
+                }
+            }
+            if ($userCanEditOffices && $row['member_person_id']) {
+                $actions[] = [
+                    'url'   => parent::generateUri('offices.update')."?committee_id={$committee->getId()};person_id=$row[member_person_id]",
+                    'label' => $this->_('office_add')
+                ];
+            }
+
+            $seat_data[$i]['actionLinks'] = $actions;
+        }
     }
 }
