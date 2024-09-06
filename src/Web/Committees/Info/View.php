80edbc06f8fdba6a551f35dea8f95ce65cbe4dc5
Cliff Ingham <inghamn@bloomington.in.gov>
2024-09-06 11:09:34 -0400

    Committee::info now lists liaisons

--- src/Web/Committees/Info/View.php
+++ src/Web/Committees/Info/View.php
@@ -7,36 +7,73 @@ declare (strict_types=1);
 namespace Web\Committees\Info;
 
 use Application\Models\Committee;
+use Application\Models\LiaisonTable;
 
 class View extends \Web\View
 {
     public function __construct(Committee $committee)
     {
         parent::__construct();
-        $links = [];
-        if (parent::isAllowed('committees', 'update')) {
-            $links[] = [
-                'url'   => parent::generateUri('committees.update').'?committee_id='.$committee->getId(),
-                'label' => parent::_('edit'),
-                'class' => 'edit'
-            ];
-        }
-        if (parent::isAllowed('committees', 'end')) {
-            $links[] = [
-                'url'    => parent::generateUri('committees.end').'?committee_id='.$committee->getId(),
-                'label'  => parent::_('committee_end'),
-                'class'  => 'end'
-            ];
-        }
+        $committee_id = (int)$committee->getId();
 
         $this->vars = [
             'committee'   => $committee,
-            'actionLinks' => $links
+            'liaisons'    => $this->liaisonData($committee_id),
+            'actionLinks' => $this->actionLinks($committee_id)
         ];
     }
 
     public function render(): string
     {
         return $this->twig->render("{$this->outputFormat}/committees/info.twig", $this->vars);
+    }
+
+    private function liaisonData(int $committee_id): array
+    {
+        $canEdit = parent::isAllowed('liaisons', 'update');
+        $canDel  = parent::isAllowed('liaisons', 'delete');
+        $res     = LiaisonTable::committeeLiaisonData(['committee_id'=>$committee_id]);
+        $data    = [];
+        foreach ($res['results'] as $row) {
+            $links = [];
+            if ($canEdit) {
+                $links[] = [
+                    'url'   => parent::generateUri('liaisons.update')."?liasion_id=$row[liaison_id]",
+                    'label' => _('liaison_edit'),
+                    'class' => 'edit'
+                ];
+            }
+            if ($canDel) {
+                $links[] = [
+                    'url'   => parent::generateUri('liaisons.delete')."?liasion_id=$row[liaison_id]",
+                    'label' => _('liaison_delete'),
+                    'class' => 'delete'
+                ];
+            }
+            $row['actionLinks'] = $links;
+            $data[] = $row;
+        }
+        return $data;
+    }
+
+    private function actionLinks(int $committee_id): array
+    {
+        $links = [];
+        $param = "?committee_id=$committee_id";
+        if (parent::isAllowed('committees', 'update')) {
+            $links[] = [
+                'url'   => parent::generateUri('committees.update').$param,
+                'label' => parent::_('edit'),
+                'class' => 'edit'
+            ];
+        }
+        if (parent::isAllowed('committees', 'end')) {
+            $links[] = [
+                'url'    => parent::generateUri('committees.end').$param,
+                'label'  => parent::_('committee_end'),
+                'class'  => 'end'
+            ];
+        }
+        return $links;
     }
 }
