f922a129d42b27d27f307cecb390b908903b210e
Cliff Ingham <inghamn@bloomington.in.gov>
2024-09-13 15:20:26 -0400

    Appointers: moved permission checking into View class

--- src/Web/Appointers/List/View.php
+++ src/Web/Appointers/List/View.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright 2014-2023 City of Bloomington, Indiana
+ * @copyright 2014-2024 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
  */
 declare(strict_types=1);
@@ -11,11 +11,48 @@ class View extends \Web\View
     public function __construct($appointers)
     {
         parent::__construct();
-        $this->vars['appointers'] = $appointers;
+
+        $this->vars = [
+            'appointers'  => $this->appointer_data($appointers),
+            'actionLinks' => $this->actionLinks()
+        ];
     }
 
     public function render(): string
     {
         return $this->twig->render('html/appointers/list.twig', $this->vars);
+    }
+
+    private function appointer_data($appointers): array
+    {
+        $out     = [];
+        $canEdit = parent::isAllowed('appointers', 'update');
+        foreach ($appointers as $a) {
+            $links = [];
+            if ($canEdit) {
+                $links[] = [
+                    'url'   => parent::generateUri('appointers.update').'?id='.$a->getId(),
+                    'label' => parent::_('appointer_edit'),
+                    'class' => 'edit'
+                ];
+            }
+            $out[] = [
+                'name'         => $a->getName(),
+                'actionLinks' => $links
+            ];
+        }
+        return $out;
+    }
+
+    private function actionLinks(): array
+    {
+        if (parent::isAllowed('appointers', 'update')) {
+            return [[
+                'url'   => parent::generateUri('appointers.update'),
+                'label' => parent::_('appointer_add'),
+                'class' => 'add'
+            ]];
+        }
+        return [];
     }
 }
