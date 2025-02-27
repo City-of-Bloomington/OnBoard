09e2bf7e71cb3d3985b2df22a6f6b984fedfe238
Cliff Ingham <inghamn@bloomington.in.gov>
2025-02-27 14:19:06 -0500

    Fixed committees::add
    The add committee screen cannot use the committee layout, as it does not actually have a committee.  The side-nav and committee header require a valid committee.

--- src/Web/Committees/Add/View.php
+++ src/Web/Committees/Add/View.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright 2024 City of Bloomington, Indiana
+ * @copyright 2024-2025 City of Bloomington, Indiana
  * @license https://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
  */
 declare (strict_types=1);
@@ -26,6 +26,6 @@ class View extends \Web\View
 
     public function render(): string
     {
-        return $this->twig->render('html/committees/updateForm.twig', $this->vars);
+        return $this->twig->render('html/committees/addForm.twig', $this->vars);
     }
 }
