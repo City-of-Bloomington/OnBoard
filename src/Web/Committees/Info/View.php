b801ffb4d17daf23e4b71936912a45a3d16ecfb3
Cliff Ingham <inghamn@bloomington.in.gov>
2025-05-02 14:56:03 -0400

    Committee::end now uses form macros
    Updates #394

--- src/Web/Committees/Info/View.php
+++ src/Web/Committees/Info/View.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright 2024 City of Bloomington, Indiana
+ * @copyright 2024-2025 City of Bloomington, Indiana
  * @license https://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
  */
 declare (strict_types=1);
@@ -13,11 +13,10 @@ class View extends \Web\View
     public function __construct(Committee $committee)
     {
         parent::__construct();
-        $committee_id = (int)$committee->getId();
 
         $this->vars = [
             'committee'    => $committee,
-            'actionLinks'  => $this->actionLinks ($committee_id),
+            'actionLinks'  => self::actionLinks($committee),
         ];
     }
 
@@ -26,19 +25,19 @@ class View extends \Web\View
         return $this->twig->render("{$this->outputFormat}/committees/info.twig", $this->vars);
     }
 
-    private function actionLinks(int $committee_id): array
+    private static function actionLinks(Committee $c): array
     {
         $links = [];
         if (parent::isAllowed('committees', 'update')) {
             $links[] = [
-                'url'   => parent::generateUri('committees.update', ['id'=>$committee_id]),
+                'url'   => parent::generateUri('committees.update', ['id'=>$c->getId()]),
                 'label' => parent::_('edit'),
                 'class' => 'edit'
             ];
         }
-        if (parent::isAllowed('committees', 'end')) {
+        if (parent::isAllowed('committees', 'end') && !$c->getEndDate()) {
             $links[] = [
-                'url'    => parent::generateUri('committees.end', ['id'=>$committee_id]),
+                'url'    => parent::generateUri('committees.end', ['id'=>$c->getId()]),
                 'label'  => parent::_('committee_end'),
                 'class'  => 'delete'
             ];
