66042b855d6a43263e798f86ad062213cd2ca167
Cliff Ingham <inghamn@bloomington.in.gov>
2025-07-03 14:02:27 -0400

    Permission checking must be done in Controllers
    The Models are not the place for permission checking.  They are expected to diligently return data requested by the Controllers.  It is up to the Controller (or View) to decide what data to show.
    
    Updates #393

--- src/Application/Models/PeopleTable.php
+++ src/Application/Models/PeopleTable.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @copyright 2013-2016 City of Bloomington, Indiana
- * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ * @copyright 2013-2025 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
  */
 namespace Application\Models;
 
@@ -11,92 +11,69 @@ use Laminas\Db\Sql\Predicate\Like;
 
 class PeopleTable extends TableGateway
 {
-    public static $columns = ['firstname', 'lastname'];
+    public static $columns = ['firstname', 'lastname', 'email', 'phone', 'username', 'department_id'];
 
     public function __construct() { parent::__construct('people', __namespace__.'\Person'); }
 
     public function find($fields=null, $order='lastname', $paginated=false, $limit=null)
     {
-        $select = new Select('people');
+        $select = new Select(['p'=>'people']);
         $select->quantifier(Select::QUANTIFIER_DISTINCT);
 
         if ($fields) {
-            foreach ($fields as $key=>$value) {
-                switch ($key) {
+            foreach ($fields as $k=>$v) {
+                switch ($k) {
                     case 'user_account':
-                        if ($value) {
-                            $select->where('username is not null');
-                        }
-                        else {
-                            $select->where('username is null');
-                        }
+                        if ($v) { $select->where('p.username is not null'); }
+                        else    { $select->where('p.username is null'    ); }
                     break;
 
                     case 'liaison':
-                        if ($value) {
-                            $select->join(['l'=>'committee_liaisons'], 'people.id=l.person_id', []);
+                        if ($v) {
+                            $select->join(['l'=>'committee_liaisons'], 'p.id=l.person_id', []);
                         }
                     break;
 
                     case 'committee_id':
-                        $select->join(['t'=>'terms'], 'people.id=t.person_id', []);
-                        $select->join(['s'=>'seats'], 't.seat_id=s.id',        []);
-                        $select->where(['s.committee_id'=>$value]);
-                    break;
-
-                    case 'email':
-                    case 'phone':
-                        if (Person::isAllowed('people', 'viewContactInfo')) {
-                            $select->where([$key=>$value]);
-                        }
-                    break;
-
-                    case 'department_id':
-                        if ($value) { $select->where([$key=>$value]); }
+                        $select->join(['t'=>'terms'], 'p.id=t.person_id', []);
+                        $select->join(['s'=>'seats'], 't.seat_id=s.id',   []);
+                        $select->where(['s.committee_id'=>$v]);
                     break;
 
                     default:
-                        if (in_array($key, self::$columns)) {
-                            $select->where([$key=>$value]);
+                        if ($v && in_array($k, self::$columns)) {
+                            $select->where(["p.$k"=>$v]);
                         }
                 }
             }
         }
+
+        $order = 'p.'.$order;
         return parent::performSelect($select, $order, $paginated, $limit);
     }
 
     public function search($fields, $order='lastname', $paginated=false, $limit=null)
     {
-        $select = new Select('people');
+        $select = new Select(['p'=>'people']);
         foreach ($fields as $k => $v) {
             switch ($k) {
                 case 'user_account':
-                    if ($v) { $select->where('username is not null'); }
-                    else    { $select->where('username is null'    ); }
+                    if ($v) { $select->where('p.username is not null'); }
+                    else    { $select->where('p.username is null'    ); }
                 break;
 
                 case 'role':
                 case 'department_id':
-                    if ($v) { $select->where([$k=>$v]); }
-                break;
-
-                case 'username':
-                    if ($v) { $select->where->like($k, "$v%"); }
-                break;
-
-                case 'email':
-                case 'phone':
-                    if ($v && Person::isAllowed('people', 'viewContactInfo')) {
-                        $select->where->like($k, "$v%");
-                    }
+                    if ($v) { $select->where(["p.$k"=>$v]); }
                 break;
 
                 default:
                     if ($v && in_array($k, self::$columns)) {
-                        $select->where->like($k, "$v%");
+                        $select->where->like("p.$k", "$v%");
                     }
             }
         }
+        $order = 'p.'.$order;
         return parent::performSelect($select, $order, $paginated, $limit);
     }
 }
