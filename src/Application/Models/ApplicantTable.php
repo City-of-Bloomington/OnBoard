d3b76b864cb2e1043fa18ecd90d8b088ca64ea03
Cliff Ingham <inghamn@bloomington.in.gov>
2025-07-07 09:40:16 -0400

    Applicants: added search form
    The big long list of applicants is not very useful.  This adds a paginated search through the applicants.
    
    Updates #435

--- src/Application/Models/ApplicantTable.php
+++ src/Application/Models/ApplicantTable.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright 2016-2020 City of Bloomington, Indiana
+ * @copyright 2016-2025 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
  */
 declare (strict_types=1);
@@ -13,6 +13,22 @@ use Laminas\Db\Sql\Select;
 class ApplicantTable extends TableGateway
 {
     public function __construct() { parent::__construct('applicants', __namespace__.'\Applicant'); }
+
+    public static $fields = ['firstname', 'lastname', 'email'];
+
+    public function search($fields=null, $order='name', $paginated=false, $limit=null)
+    {
+        $select = new Select('applicants');
+
+        if ($fields) {
+            foreach ($fields as $k=>$v) {
+                if ($v && in_array($k, self::$fields)) {
+                    $select->where->like($k, "$v%");
+                }
+            }
+        }
+        return parent::performSelect($select, $order, $paginated, $limit);
+    }
 
     /**
      * Check if the user shares a committee with the applicant
