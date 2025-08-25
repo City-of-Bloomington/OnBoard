505831347c7d8b4fdca4fbdaccb482adbf436bfe
Cliff Ingham <inghamn@bloomington.in.gov>
2025-08-25 12:51:49 -0400

    Committee::data now only counts current seats
    Updates #451

--- src/Application/Models/Committee.php
+++ src/Application/Models/Committee.php
@@ -402,7 +402,7 @@ class Committee extends ActiveRecord
                             where a.committee_id=c.id and (a.archived is null or now() <= a.archived)
                         ) as applications
                 from committees c
-                left join seats s    on c.id= s.committee_id
+                left join seats s    on c.id= s.committee_id and (s.endDate is null or now()<=s.endDate)
                 left join members m  on s.id= m.seat_id and  (m.startDate is null or  m.startDate <= now()) and ( m.endDate is null or now() <= m.endDate)
                 left join terms   t  on s.id= t.seat_id and  (t.startDate is null or  t.startDate <= now()) and ( t.endDate is null or now() <= t.endDate)
                 left join members tm on t.id=tm.term_id and (tm.startDate is null or tm.startDate <= now()) and (tm.endDate is null or now() <=tm.endDate)
