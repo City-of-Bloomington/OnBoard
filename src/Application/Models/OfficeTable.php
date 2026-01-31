86cf946ab2499aada3b3b8b7dfb06ae27c144d8c
Cliff Ingham <inghamn@bloomington.in.gov>
2026-01-31 09:36:58 -0500

    Coverted Table classes to PDO
    Updates #483

--- src/Application/Models/OfficeTable.php
+++ src/Application/Models/OfficeTable.php
@@ -1,46 +1,51 @@
 <?php
 /**
- * @copyright 2014-2025 City of Bloomington, Indiana
- * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ * @copyright 2014-2026 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
  */
 namespace Application\Models;
 
-use Web\Database;
-use Web\TableGateway;
-use Laminas\Db\Sql\Select;
+use Application\PdoRepository;
 
-class OfficeTable extends TableGateway
+class OfficeTable extends PdoRepository
 {
     public function __construct() { parent::__construct('offices', __namespace__.'\Office'); }
 
-    public function find(?array $fields=null, string|array|null $order='startDate', ?int $itemsPerPage=null, ?int $currentPage=null): array
+    public function find(array $fields=[], ?string $order='startDate', ?int $itemsPerPage=null, ?int $currentPage=null): array
     {
-        $select = new Select('offices');
+        $select = 'select * from offices';
+        $joins  = [];
+        $where  = [];
+        $params = [];
+
         if ($fields) {
-            foreach ($fields as $key=>$value) {
-                switch ($key) {
+            foreach ($fields as $k=>$v) {
+                switch ($k) {
                     case 'current':
-                        $date = \DateTime::createFromFormat('Y-m-d', $value);
-                        $select->where("startDate<='{$date->format('Y-m-d')}'");
-                        $select->where("(endDate is null or endDate>='{$date->format('Y-m-d')}')");
+                        $date    = \DateTime::createFromFormat('Y-m-d', $v);
+                        $where[] = "startDate<='{$date->format('Y-m-d')}'";
+                        $where[] = "(endDate is null or endDate>='{$date->format('Y-m-d')}')";
                         break;
 
                     default:
-                        $select->where([$key=>$value]);
+                        $where[] = "$k=:$k";
+                        $params[$k] = $v;
                 }
             }
         }
-        return parent::performSelect($select, $order, $itemsPerPage, $currentPage);
+        $sql  = parent::buildSql($select, $joins, $where, null, $order);
+        return  parent::performSelect($sql, $params, $itemsPerPage, $currentPage);
     }
 
-    public static function hasDepartment(int $department_id, int $office_id): bool
+    public function hasDepartment(int $department_id, int $office_id): bool
     {
         $sql    = "select o.committee_id
                    from offices o
                    join committee_departments d on o.committee_id=d.committee_id
                    where d.department_id=? and o.id=?";
-        $db     = Database::getConnection();
-        $result = $db->query($sql)->execute([$department_id, $office_id]);
+        $query  = $this->pdo->prepare($sql);
+        $query->execute([$department_id, $office_id]);
+        $result = $query->fetchAll(\PDO::FETCH_ASSOC);
         return count($result) ? true : false;
     }
 }
